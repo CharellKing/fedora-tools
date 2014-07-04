68498bf22b58e29aa1cd126f2024f2f0173f5bde
CharellKing <CharellkingQu@gmail.com>
2014-07-04 19:57:18 +0800

    install emacs and need proxy

---
+++ fedora-emacs.sh
@@ -0,0 +1,16 @@
+sudo yum install python-pip -y
+sudo pip install cpplint
+
+sudo yum install ruby -y
+sudo yum install git -y
+sudo yum install doxygen -y
+
+sudo yum install gcc g++ -y
+sudo yum install cmake
+
+sudo pip install ipython
+
+sudo yum install emacs
+
+#start emacs to install package with proxy
+http_proxy=127.0.0.1:8086 emacs -nw
