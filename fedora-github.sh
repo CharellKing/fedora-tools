c3ba67a8a4ffdfb17a37cfca4d37841a3df33532
CharellKing <CharellkingQu@gmail.com>
2014-07-04 19:58:52 +0800

    configure github and need add rsa-pub in github ssh-setting yourself

---
+++ fedora-github.sh
@@ -0,0 +1,11 @@
+sudo yum install git -y
+ssh-keygen -t rsa -C "CharellkingQu@gmail.com"
+sudo yum install xsel -y
+cat ~/.ssh/id_rsa.pub | xsel -b -i
+echo "please paste public-key to github's setting, then any key for contiune"
+read
+ssh -T git@github.com
+git config --global user.name "CharellKing"
+git config --global user.email "CharellkingQu@gmail.com"
+cd ~
+git clone https://github.com/CharellKing/.emacs.d.git
